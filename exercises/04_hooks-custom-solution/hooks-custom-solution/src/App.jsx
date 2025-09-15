352ddceaa07893ed47206a2d3e648770898b33c5
Arthur Baars <arthurbaars@grachtwerk.nl>
2025-09-15 10:23:36 +0200

    Extended 04_custom-hooks-solution

--- exercises/04_hooks-custom-solution/hooks-custom-solution/src/App.jsx
+++ exercises/04_hooks-custom-solution/hooks-custom-solution/src/App.jsx
@@ -2,15 +2,22 @@ import {useLocalStorage } from "./useLocalStorage";
 
 export const App = () => {
     const [name, setName] = useLocalStorage("name", "John");
+    const [hobby, setHobby] = useLocalStorage("hobby", "Coding");
 
     return (
         <>
             <h1>Welcome, {name}</h1>
+            <p>{name} likes {hobby}</p>
 
             <label>Name:</label>
             <input
                 value={name}
                 onChange={(e) => setName(e.target.value)}
+            />
+            <label>Hobby:</label>
+            <input
+                value={hobby}
+                onChange={(e) => setHobby(e.target.value)}
             />
         </>
     );
