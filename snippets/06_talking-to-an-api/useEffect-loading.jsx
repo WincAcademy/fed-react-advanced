b7c1d7c167df2fb6921098dc8565bc7ca20ab456
Arthur Baars <arthurbaars@grachtwerk.nl>
2025-09-24 10:27:20 +0200

    Added code snippet

--- snippets/06_talking-to-an-api/useEffect-loading.jsx
+++ snippets/06_talking-to-an-api/useEffect-loading.jsx
@@ -1,8 +1,20 @@
+// We define a loading state, by default false
+const [loading, setLoading] = useState(false);
+
 useEffect(() => {
+    // Before we make any asynchronous request,
+    // we set loading to true
+    setLoading(true);
+
+    // Continue as normal
     const fetchNotes = async () => {
         const response = await fetch('http://localhost:3000/notes');
         const json = await response.json();
         setNotes(json);
+
+        // After the asynchronous wait, reset
+        // loading back to false
+        setLoading(false);
     };
     fetchNotes();
 }, []);
